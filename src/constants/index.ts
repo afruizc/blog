edf82bef021dd84e09bf78a74fa97c212dc7371b
green-john <andresfeliperu@gmail.com>
2021-04-06 16:08:41 +0200

    add new blog

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,9 @@
 const ENTRY_0 = '2020-legacy-code-case-study';
 const ENTRY_1 = '2020-my-journey-through-the-functional-world';
+const ENTRY_2 = '2020-my-journey-through-the-functional-world-local';
 
 export const ENTRIES = new Map<string, string>([
-    [ENTRY_0, 'https://raw.githubusercontent.com/green-john/blog/main/blogs/2020-legacy-code-case-study'],
+    [ENTRY_2, 'file:///Users/afruizc/Documents/code/green-john/blogs/2020-journey-functional-world/README.md'],
     [ENTRY_1, 'https://raw.githubusercontent.com/green-john/blog/main/blogs/2020-journey-functional-world'],
+    [ENTRY_0, 'https://raw.githubusercontent.com/green-john/blog/main/blogs/2020-legacy-code-case-study'],
 ]);
